685b80addbd4c3640a8643a7decc51b27f6d9c60
tr0jek <53969931+tr0jek@users.noreply.github.com>
2023-05-30 18:35:59 +0200

    Create method new teacher

--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -3,13 +3,15 @@ import getStatus from './status/get.status'
 import postUser from './user/post.user'
 import postStudent from './student/post.student'
 import getLoginUser from './user/login.user'
+import postTeacher from './teacher/post.teacher'
 const router = express.Router()
 // home page route
 router.get('/', (req, res) => {
     res.send('Example home page')
 })
-const apiRoutes = [getStatus, postUser, getLoginUser, postStudent]
+const apiRoutes = [getStatus, postUser, getLoginUser, postStudent, postTeacher]
 apiRoutes.forEach((route) =>
     router[route.method](route.path, route.validators, route.handler),
 )
+
 export default router
---
+++ src/backend/routes/teacher/post.teacher.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express'
+import { body } from 'express-validator'
+import { StatusCodes } from 'http-status-codes'
+import { v4 } from 'uuid'
+import { prisma } from '../../database'
+import { TRoute } from '../types'
+import { handleRequest } from '../../utils/request.utils'
+import { createHash } from '../../utils/hash.utils'
+import { authorize } from '../../utils/middleware.utils'
+const SALT = (process.env.PASSWORD_SALT as string) ?? 'XYZ'
+
+//Creates User and Student entity
+export default {
+    method: 'post',
+    path: '/api/teacher',
+    validators: [
+        authorize,
+        body('Email').isEmail(),
+        body('Password').not().isEmpty(),
+    ],
+    handler: async (req: Request, res: Response) =>
+        handleRequest({
+            req,
+            res,
+            responseSuccessStatus: StatusCodes.CREATED,
+            messages: { uniqueConstraintFailed: 'Email must be unique.' },
+            execute: async () => {
+                const { Email, Name, Password } = req.body
+                const passwordHash = createHash(Password, SALT)
+                const user = await prisma.user.create({
+                    data: {
+                        UserID: v4(),
+                        Name,
+                        Email,
+                        Password: passwordHash,
+                    },
+                })
+                return await prisma.teacher.create({
+                    data: {
+                        TeacherID: v4(),
+                        UserID: user.UserID,
+                    },
+                })
+            },
+        }),
+} as TRoute
