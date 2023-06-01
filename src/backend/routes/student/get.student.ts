b0ce17451aea4ea577e2e83335b0627416c0b36b
Dominik Soj <48448459+WestHumble@users.noreply.github.com>
2023-06-01 16:41:07 +0200

    GET student

---
+++ src/backend/routes/student/get.student.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { prisma } from '../../database'
+import { TRoute } from '../types'
+import { handleRequest } from '../../utils/request.utils'
+import { authorize } from '../../utils/middleware.utils'
+
+export default {
+    method: 'get',
+    path: '/api/student/get-student',
+    validators: [authorize],
+    handler: async (req: Request, res: Response) =>
+        handleRequest({
+            req,
+            res,
+            responseSuccessStatus: StatusCodes.OK,
+            execute: async () => {
+                const { StudentID } = req.body
+                return await prisma.student.findUnique({
+                    where: {
+                        StudentID: StudentID,
+                    },
+                })
+            },
+        }),
+} as TRoute
