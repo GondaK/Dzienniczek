95cc55a986350416818930b1b25268bc9ac72c7e
kgondaCDV <kgonda@edu.cdv.com>
2023-06-01 18:56:58 +0200

    add weather endpoint

---
+++ src/backend/routes/weather/get.weather.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { TRoute } from '../types'
+import { handleRequest } from '../../utils/request.utils'
+import * as http from 'http'
+
+export default {
+    method: 'get',
+    path: '/api/weather',
+    validators: [],
+    handler: async (req: Request, res: Response) =>
+        handleRequest({
+            req,
+            res,
+            responseSuccessStatus: StatusCodes.OK,
+            execute: async () => {
+                const params = {
+                    host: 'api.open-meteo.com',
+                    port: 80,
+                    path: '/v1/forecast?latitude=52.41&longitude=16.93&hourly=temperature_2m',
+                    method: 'GET',
+                }
+
+                return await httpRequest(params).then(function (body: any) {
+                    const mapArrays = (arr1 = [], arr2 = []) => {
+                        const res = arr1.reduce((acc, elem, index) => {
+                            acc[elem] = arr2[index]
+                            return acc
+                        }, {})
+                        return res
+                    }
+                    return mapArrays(
+                        body.hourly.time,
+                        body.hourly.temperature_2m,
+                    )
+                })
+            },
+        }),
+} as TRoute
+
+function httpRequest(params: any, postData = null) {
+    return new Promise(function (resolve, reject) {
+        const req = http.request(params, function (res) {
+            let body: any[] = []
+            res.on('data', function (chunk) {
+                body.push(chunk)
+            })
+            res.on('end', function () {
+                try {
+                    body = JSON.parse(Buffer.concat(body).toString())
+                } catch (e) {
+                    reject(e)
+                }
+                resolve(body)
+            })
+        })
+        req.on('error', function (err) {
+            reject(err)
+        })
+        if (postData) {
+            req.write(postData)
+        }
+
+        req.end()
+    })
+}
